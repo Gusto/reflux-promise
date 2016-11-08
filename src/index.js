a71e221f75e2df65ef2e2bb1f88a776fb0a47173
Rylan Collins <rylan@zenpayroll.com>
2016-11-07 16:07:01 -0800

    Immediately dispatch actions

--- src/index.js
+++ src/index.js
@@ -1,60 +1,56 @@
 function createFunctions(Reflux, PromiseFactory, catchHandler) {
-  const _ = Reflux.utils;
-
-    /**
-     * Returns a Promise for the triggered action
-     *
-     * @return {Promise}
-     *   Resolved by completed child action.
-     *   Rejected by failed child action.
-     *   If listenAndPromise'd, then promise associated to this trigger.
-     *   Otherwise, the promise is for next child action completion.
-     */
+  /**
+   * Returns a Promise for the triggered action
+   *
+   * @return {Promise}
+   *   Resolved by completed child action.
+   *   Rejected by failed child action.
+   *   If listenAndPromise'd, then promise associated to this trigger.
+   *   Otherwise, the promise is for next child action completion.
+   */
   function triggerPromise(...triggerArgs) {
     const canHandlePromise =
             this.children.indexOf('completed') >= 0 &&
             this.children.indexOf('failed') >= 0;
 
     const createdPromise = new PromiseFactory((resolve, reject) => {
-            // If `listenAndPromise` is listening
-            // patch `promise` w/ context-loaded resolve/reject
+      // If `listenAndPromise` is listening
+      // patch `promise` w/ context-loaded resolve/reject
       if (this.willCallPromise) {
-        _.nextTick(() => {
-          const previousPromise = this.promise;
-          this.promise = (inputPromise, ...promiseArgs) => {
-            inputPromise.then(resolve, reject);
-                        // Back to your regularly schedule programming.
-            this.promise = previousPromise;
-            return this.promise(inputPromise, ...promiseArgs);
-          };
-          this.trigger(...triggerArgs);
-        });
+        const previousPromise = this.promise;
+        this.promise = (inputPromise, ...promiseArgs) => {
+          inputPromise.then(resolve, reject);
+          // Back to your regularly schedule programming.
+          this.promise = previousPromise;
+          return this.promise(inputPromise, ...promiseArgs);
+        };
+        this.trigger(...triggerArgs);
 
         return;
       }
 
       if (canHandlePromise) {
-        const removeSuccess = this.completed.listen((...args) => {
-          removeSuccess();
-          removeFailed();
+        const unsubscribes = [];
+        const unsubscribe = () => unsubscribes.forEach(unsub => unsub());
+        unsubscribes.push(this.completed.listen((...args) => {
+          unsubscribe();
           resolve(args.length > 1 ? args : args[0]);
-        });
+        }));
 
-        const removeFailed = this.failed.listen((...args) => {
-          removeSuccess();
-          removeFailed();
+        unsubscribes.push(this.failed.listen((...args) => {
+          unsubscribe();
           reject(args.length > 1 ? args : args[0]);
-        });
+        }));
       }
 
-      _.nextTick(() => this.trigger(...triggerArgs));
+      this.trigger(...triggerArgs);
 
       if (!canHandlePromise) {
         resolve();
       }
     });
 
-        // Attach promise catch handler if provided
+    // Attach promise catch handler if provided
     if (typeof (catchHandler) === 'function') {
       createdPromise.catch(catchHandler);
     }
@@ -62,12 +58,12 @@ function createFunctions(Reflux, PromiseFactory, catchHandler) {
     return createdPromise;
   }
 
-    /**
-     * Attach handlers to promise that trigger the completed and failed
-     * child publishers, if available.
-     *
-     * @param {Object} p The promise to attach to
-     */
+  /**
+   * Attach handlers to promise that trigger the completed and failed
+   * child publishers, if available.
+   *
+   * @param {Object} p The promise to attach to
+   */
   function promise(p) {
     const canHandlePromise =
             this.children.indexOf('completed') >= 0 &&
@@ -80,12 +76,12 @@ function createFunctions(Reflux, PromiseFactory, catchHandler) {
     p.then(response => this.completed(response), error => this.failed(error));
   }
 
-    /**
-     * Subscribes the given callback for action triggered, which should
-     * return a promise that in turn is passed to `this.promise`
-     *
-     * @param {Function} callback The callback to register as event handler
-     */
+  /**
+   * Subscribes the given callback for action triggered, which should
+   * return a promise that in turn is passed to `this.promise`
+   *
+   * @param {Function} callback The callback to register as event handler
+   */
   function listenAndPromise(callback, bindContext) {
     bindContext = bindContext || this;
     this.willCallPromise = (this.willCallPromise || 0) + 1;
@@ -106,9 +102,9 @@ function createFunctions(Reflux, PromiseFactory, catchHandler) {
   }
 
   return {
-    triggerPromise: triggerPromise,
-    promise: promise,
-    listenAndPromise: listenAndPromise
+    triggerPromise,
+    promise,
+    listenAndPromise
   };
 }
 
